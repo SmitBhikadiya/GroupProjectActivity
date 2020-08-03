ec359fda0ed222d3b0e84f4e8c6adb73e1ab1872
Smit Patel <sbhikadiya892@rku.ac.in>
2020-08-03 18:33:44 +0530

    Version 1.1

--- app/src/main/java/com/spatel/groupapplication/LoginActivity.java
+++ app/src/main/java/com/spatel/groupapplication/LoginActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class LoginActivity extends AppCompatActivity {
 
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //This method is used so that your splash activity
+
     }
+
 }
